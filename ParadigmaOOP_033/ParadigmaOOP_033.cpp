715d85b729721dbc1c3798b4dc1c12efd81ca914
defarrel <Defarrel>
2023-05-24 15:19:11 +0700

    membuat class pelajar dari keturunan class orang dengan member sekolah dan konstruktor

--- ParadigmaOOP_033/ParadigmaOOP_033.cpp
+++ ParadigmaOOP_033/ParadigmaOOP_033.cpp
@@ -18,3 +18,12 @@ public:
 	}
 };
 
+class pelajar : public orang {
+public :
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibut\n" << endl;
+	}
